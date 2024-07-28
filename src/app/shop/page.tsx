37d42b701b63adbb43a8e4f2ca2ecd2fcd40713c
laon6 <laon6.dev@gmail.com>
2024-07-28 22:20:23 +0900

    feature/add-navbar

---
+++ src/app/shop/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Shop() {
+
+    return (
+        <>
+            shop
+        </>
+    );
+
+}
---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,15 @@
+// src/components/common/Footer.tsx
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <></> // TODO
+        // <footer className="bg-gray-800 text-white py-4">
+        //     <div className="container mx-auto text-center">
+        //         <p>&copy; {new Date().getFullYear()} MySite. All rights reserved.</p>
+        //     </div>
+        // </footer>
+    );
+};
+
+export default Footer;
